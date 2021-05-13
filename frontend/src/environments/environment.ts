f06acda50d4056445e7064a8e309f0e90f2304cf
Lia Gaetano <lgaetano19@gmail.com>
2021-05-13 15:52:59 -0700

    updated formatting

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,14 +1,10 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
- */
-
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'http://127.0.0.1:5000',
   auth0: {
-    url: 'lgaetano.us.auth0.com', // the auth0 domain prefix
-    audience: 'drinks', // the audience set for the auth0 app
-    clientId: 'uXM3OdkkYavBmQNO0ZtNuYk7bOnmHEbQ', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'lgaetano.us.auth0.com',
+    audience: 'drinks',
+    clientId: 'uXM3OdkkYavBmQNO0ZtNuYk7bOnmHEbQ',
+    callbackURL: 'http://localhost:8100',
   }
 };
